81c845c73d895d1e6ddfb823564484ef630e8971
aramsay03 <alan@aramsay.co.uk>
2020-11-16 11:37:05 +0000

    Instruction section Baxk and Next buttons working

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,15 +1,56 @@
-import React from 'react';
-// import { BrowserRouter as Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, Redirect, Route } from 'react-router-dom';
 import classes from './Instructions.module.css';
 
 const Instructions = ({ selectedDay }) => {
 
     // Need to add back button
-
+        // If statement for when section is 1 (return to home page)
     // Need to create next button
+        // If statement for when section is at last for that day (return to home page)
 
-    let currentSection = 1;
-    const imageURL = `./images/instructions/${selectedDay.folder}/${selectedDay.folder}-${currentSection}.png`;
+    const [section, setSection] = useState(1);
+    const imageURL = `./images/instructions/${selectedDay.folder}/${selectedDay.folder}-${section}.png`;
+
+    const backSection = () => {
+        setSection(section - 1)
+    }
+
+    const nextSection = () => {
+        setSection(section + 1)
+    }
+
+    const backButtonClick = (
+        <button onClick={backSection}>
+            Back
+        </button>
+    );
+
+    const backButton = (
+        <button>
+            Back
+        </button>
+    );
+
+    const nextButtonClick = (
+        <button onClick={nextSection}>
+            Next
+        </button>
+    );
+
+    const nextButton = (
+        <button>
+            Next
+        </button>
+    );
+
+    const ConditionalLinkBack = () => (section === 1)
+      ? <Link to='/'>{backButton}</Link>
+      : <>{backButtonClick}</>;
+    
+    const ConditionalLinkNext = () => (section >= selectedDay.numberOfSections)
+    ? <Link to='/'>{nextButton}</Link>
+    : <>{nextButtonClick}</>;
 
     return (
         <section className={classes.instructionsContainer}>
@@ -17,13 +58,13 @@ const Instructions = ({ selectedDay }) => {
                 <button onClick={this.backPage}>{this.nextPage}</button>
             </div> */}
             <div>
-                <h1>This should be instructions here</h1>
+                <h1>Section {section} of {selectedDay.numberOfSections}</h1>
                 <p>{selectedDay.folder}</p>
-                {/* {sectionImage} */}
                 <img className={classes.imageSection} src={imageURL} alt="InstructionImage" />
             </div>
             <div>
-                {/* <button onClick={this.nextPage}>Next</button> */}
+                <ConditionalLinkBack />
+                <ConditionalLinkNext />
             </div>
         </section>
     );
