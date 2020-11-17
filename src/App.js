95c914b826258451742d7eda43d00b701cf357ea
aramsay03 <alan@aramsay.co.uk>
2020-11-17 16:02:40 +0000

    New background image added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import Main from './components/Main';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
+    <div className="App" style={{
+      background: `url('${process.env.PUBLIC_URL}/images/lego_christmas_bg_1.png')`
+    }}>
       <Main />
     </div>
   );
