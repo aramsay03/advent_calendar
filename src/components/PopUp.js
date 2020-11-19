a07c9a819e93bebd6aff146a5edbd6bb96fd3c3e
aramsay03 <alan@aramsay.co.uk>
2020-11-19 23:08:26 +0000

    MaterialUi button added to popup

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,14 +1,33 @@
 import React from 'react';  
-import classes from './PopUp.module.css';  
+import classes from './PopUp.module.css';
+import Button from '@material-ui/core/Button';
+import CancelIcon from '@material-ui/icons/Cancel';
+// import Icon from '@material-ui/core/Icon';
+// import { makeStyles } from '@material-ui/core/styles';
 
-class Popup extends React.Component {  
+// const useStyles = makeStyles((theme) => ({
+//   button: {
+//     margin: theme.spacing(1),
+//   },
+// }));
+
+class Popup extends React.Component { 
+  
+  // const classesui = useStyles();
+
   render() {  
     return (  
       <div className={classes.popup}>  
         <div className={classes.popupInner}>
           <img alt="PopUp" src={this.props.popupImage} />
           <h1>{this.props.text}</h1>  
-          <button onClick={this.props.closePopup}>CLOSE</button>  
+          <Button
+            onClick={this.props.closePopup}
+            variant="contained"
+            color="red"
+            // className={classesui.button}
+            startIcon={<CancelIcon />}
+            >CLOSE</Button>  
         </div>  
       </div>  
     );  
